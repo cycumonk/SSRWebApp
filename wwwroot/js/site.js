8e8113b72272381166d1ae12d7d547f71af97e44
cycumonk <cycumonk@gmail.com>
2024-05-16 22:51:59 +0800

    試作登入表格

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById('registrationForm').addEventListener('submit', function (event) {
+    //阻止表單默認行為
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    document.getElementById('resultUsername').textContent = username;
+    document.getElementById('resultEmail').textContent = email;
+    document.getElementById('resultPassword').textContent = password;
+    //在同一頁呈現不跳頁
+    document.getElementById('resultContainer').style.display = 'block';
+});
