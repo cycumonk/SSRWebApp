720fb4c87b8c2f3c613e50b9ad8c1a4d3fefafd8
cycumonk <cycumonk@gmail.com>
2024-05-18 01:16:10 +0800

    success page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace SSRWebApp.Pages;
-
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    [BindProperty]
+    public string Account { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+    [BindProperty]
+    public string Email { get; set; }
+
+    [BindProperty]
+    public string Password { get; set; }
 
     public void OnGet()
     {
+        // 在此處初始化屬性等操作
+    }
 
+    public IActionResult OnPost()
+    {
+        // 在此處處理表單提交
+        return RedirectToPage("/Success", new { account = Account, email = Email, password = Password });
     }
 }
