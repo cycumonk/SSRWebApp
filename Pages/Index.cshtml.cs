f5bf88baae63d84df92946cc4efb1fb2ea8faab0
cycumonk <cycumonk@gmail.com>
2024-05-19 00:36:49 +0800

    renew Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -3,26 +3,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 public class IndexModel : PageModel
 {
-    [BindProperty]
-    public string? Account { get; set; }
-
-    [BindProperty]
-    public string? Email { get; set; }
-
-    [BindProperty]
-    public string? Password { get; set; }
-
     public void OnGet()
     {
-
+        // 無需邏輯，只是保持空方法
     }
 
-    public IActionResult OnPost()
-    {
-        if (ModelState.IsValid)
-        {
-            return RedirectToPage("Success", new { Account = Account, Email = Email, Password = Password });
-        }
-        return Page();
-    }
 }
--- Pages/Success.cshtml.cs
+++ Pages/Success.cshtml.cs
@@ -1,14 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SSRWebApp.Pages
 {
     public class SuccessModel : PageModel
     {
+        [BindProperty]
         public string? Account { get; set; }
+        [BindProperty]
         public string? Email { get; set; }
+        [BindProperty]
         public string? Password { get; set; }
 
-        public void OnGet(string account, string email, string password)
+        public void OnPost(string account, string email, string password)
         {
             Account = account;
             Email = email;
